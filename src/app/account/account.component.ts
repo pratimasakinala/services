9bf1a6023207ebc8c331894aca2176b3b0c51dfc
Pratima Sakinala <pratimasakinala@Pratimas-MacBook-Pro.local>
2018-06-22 15:28:18 -0700

    creating a loggingService to understand basics of services and way to use it in a component

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -10,9 +10,7 @@ export class AccountComponent {
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
   }
 }
